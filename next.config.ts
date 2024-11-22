3a56600adfaab3f58a08a1d44daa3b5e57f2c160
natthawat141 <bill.natthawat@gmail.com>
2024-11-22 16:14:58 +0700

    commit voice

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  
+  "scripts": {
+    "build": "next build --force"
+  }
   
 };
 
