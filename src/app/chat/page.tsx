5cdb60f90af15081192e511fb59b334e693790b4
natthawat141 <bill.natthwat@gmail.com>
2024-12-20 09:41:43 +0700

    commit successful

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -344,7 +344,8 @@ export default function ChatPage() {
       </main>
 
       {/* QuickPdfSelector with toggle button */}
-      <div className="absolute bottom-16 sm:bottom-24 left-1/2 -translate-x-1/2 z-10 mb-10 md:mb-0">
+      {/* mb-10 md:mb-0 */}
+      <div className="absolute bottom-16 sm:bottom-24 left-1/2 -translate-x-1/2 z-10 ">
         <AnimatePresence>
           {showPdfSelector && (
             <motion.div
